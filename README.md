069d6126f1afa20c49d39b4ebf87503a4b4dd658
geckocoding <hello.derpdex@gmail.com>
2023-11-15 13:23:25 +0800

    Added mainnet setup

--- README.md
+++ README.md
@@ -10,3 +10,13 @@ yarn hardhat compile && yarn hardhat run scripts/deployDerp.js --network base
 # Deploy Derp to opBNB
 yarn hardhat compile && yarn hardhat run scripts/deployDerp.js --network opBnb
 ```
+
+## DERP token addresses
+
+| Blockchain | Contract Address |
+|------------|------------------|
+| Ethereum Mainnet | [0x5DfC78C4D073fD343BC6661668948178522A0DE5](https://etherscan.io/address/0x5DfC78C4D073fD343BC6661668948178522A0DE5) |
+| BNB Chain | [0xEbb78043e29F4af24E6266A7D142f5A08443969E](https://bscscan.com/address/0xebb78043e29f4af24e6266a7d142f5a08443969e) |
+| ZkSync Era | [0x0bf4CB727b3f8092534D793893B2cC3348963dbf](https://explorer.zksync.io/address/0x0bf4CB727b3f8092534D793893B2cC3348963dbf) |
+| opBNB | [0xEbb78043e29F4af24E6266A7D142f5A08443969E](https://opbnbscan.com/address/0xEbb78043e29F4af24E6266A7D142f5A08443969E) |
+| Base | [0xEbb78043e29F4af24E6266A7D142f5A08443969E](https://basescan.org/address/0xEbb78043e29F4af24E6266A7D142f5A08443969E) |
---
+++ scripts/deployTestnet.js
@@ -0,0 +1,64 @@
+const { ethers, upgrades, run } = require("hardhat");
+const { getImplementationAddress } = require('@openzeppelin/upgrades-core')
+
+const minRedeemRatio = "42"
+const maxRedeemRatio = "100"
+const minRedeemDuration = "30" //"2073600"
+const maxRedeemDuration = "120" //8294400"
+
+const DerpAddress = ""
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    console.log("Deployer address:", deployer.address);
+
+    // const MockERC20Factory = await ethers.getContractFactory("MOCKERC20");
+    // console.log("Deploying MockERC20...");
+    // const MockERC20 = await MockERC20Factory.deploy({/* gasPrice: 1000000000, nonce:34 */});
+    // await MockERC20.deploymentTransaction().wait(1);
+    // const MockERC20 = await ethers.getContractAt("MOCKERC20", "0xac465de4d7Fbdc2e3c33f303147298200357CbFE");
+    // console.log("MockERC20 deployed to:", await MockERC20.getAddress());
+
+    const xDERP = await ethers.getContractFactory("xDERP");
+    console.log("Deploying xDERP...");
+    const xderp = await upgrades.deployProxy(xDERP, [
+        DerpAddress,
+        minRedeemRatio,
+        maxRedeemRatio,
+        minRedeemDuration,
+        maxRedeemDuration,
+        deployer.address,
+        deployer.address
+    ], {
+        gasPrice: 11000000000,
+        // nonce: 53
+    });
+    // const xderp = await upgrades.upgradeProxy("0x93B569efC1487a3CA0b4F52d0762b81018E3ab49", xDERP, {
+    //     gasPrice: 12000000000,
+    //     nonce: 230
+    // })
+    await xderp.waitForDeployment();
+    const xDERPAddress = await xderp.getAddress();
+    console.log("xDERP deployed to:", xDERPAddress);
+    const xDERP_Implementation = await upgrades.erc1967.getImplementationAddress(xDERPAddress);
+    console.log("xDERP Implementation deployed to:", xDERP_Implementation);
+
+    //verify
+    // await run("verify:verify", {
+    //     address: await MockERC20.getAddress()
+    // })
+
+    await run("verify:verify", {
+        address: xDERP_Implementation
+    })
+
+    await run("verify:verify", {
+        address: xDERPAddress
+    })
+}
+
+
+main().then(() => process.exit(0)).catch(error => {
+    console.error(error);
+    process.exit(1);
+})
